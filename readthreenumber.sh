7878e0209f0fa27ce8b531d8fc83dbe417abae18
shreegowri <gaganandh@gmail.com>
2022-05-21 11:10:51 +0530

    [shreegowri]to compute fourth airthmatic expression

--- readthreenumber.sh
+++ readthreenumber.sh
@@ -11,6 +11,10 @@ val3=$(($a % $b ))
 val4=$(($val + $c))
 echo "$val4"
 
-val5=$(($c + $a ))
-val6=$(($val5 / $b))
+val5=$(($a * $b ))
+val6=$(($val5 - $c))
 echo "$val6"
+
+val7=$(($c + $a ))
+val8=$(($val7 / $b))
+echo "$val8"
